f41f803a4854677a7e65ff8696b900093b26fc74
BobovAA <108075070+BobovAA@users.noreply.github.com>
2023-06-19 18:59:52 +0300

    Add files via upload

---
+++ List/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+
+        list.add("Александр", "Видеооператор", 25);
+        list.add("Игорь", "Сценарист", 24);
+        list.add("Артур", "Костюмер", 26);
+        list.add("Алина", "Гримм", 26);
+        list.add("Евгения", "SFX", 25);
+        list.add("Кузьма", "Актер", 25);
+        list.print();
+    }
+}
